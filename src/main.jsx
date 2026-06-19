e31979b2838d7eba9482df84b1f4b546e67d6ad4
Siddamsetty sathish <65952467+sathish19120@users.noreply.github.com>
2026-06-19 13:11:26 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { initGA } from './lib/analytics'
+
+// Initialize Google Analytics
+initGA()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
